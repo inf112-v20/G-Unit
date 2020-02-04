483d1afa49104771d624363d6fdcff4daf6c53bf
Elias Djupesland <eliasdjup@hotmail.com>
2020-02-04 10:09:26 +0100

    Testing .tmx map loader with Tiled and LibGDX, this time using the tutorial given on mittUIB

--- src/main/java/inf112/gunit/main/Game.java
+++ src/main/java/inf112/gunit/main/Game.java
@@ -2,53 +2,44 @@ package inf112.gunit.main;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-public class Game implements ApplicationListener, InputProcessor {
-    private SpriteBatch batch;
-    private BitmapFont font;
-    Texture img;
+public class Game implements ApplicationListener {
     TiledMap tiledMap;
+    TiledMapTileLayer Board, Player, Hole, Flag;
     OrthographicCamera camera;
+    OrthogonalTiledMapRenderer tilerender;
     TiledMapRenderer tiledMapRenderer;
 
     @Override
     public void create() {
-        float w = Gdx.graphics.getWidth();
-        float h = Gdx.graphics.getHeight();
-
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false,w,h);
-        camera.update();
         tiledMap = new TmxMapLoader().load("testmap.tmx");
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-        Gdx.input.setInputProcessor(this);
+        Board = (TiledMapTileLayer) tiledMap.getLayers().get("Board");
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 5, 5);
+        camera.lookAt(2.5f, 0, 0);
+        camera.update();
+
+        tilerender = new OrthogonalTiledMapRenderer(tiledMap,1/90000 );
+
+        tilerender.setView(camera);
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        font.dispose();
     }
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        camera.update();
-        tiledMapRenderer.setView(camera);
-        tiledMapRenderer.render();
+        tilerender.render();
     }
 
     @Override
@@ -61,45 +52,5 @@ public class Game implements ApplicationListener, InputProcessor {
 
     @Override
     public void resume() {
-    }
-
-    @Override
-    public boolean keyDown(int i) {
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int i) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char c) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int i, int i1, int i2, int i3) {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int i, int i1, int i2, int i3) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int i, int i1, int i2) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int i, int i1) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(int i) {
-        return false;
     }
 }

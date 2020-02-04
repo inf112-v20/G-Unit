0cb46eac544552ffa0970ba300d14f2528d53065
Bo Aanes <boaanes@gmail.com>
2020-02-04 10:22:20 +0100

    :poop: TiledMapRenderer
    Compiles, but doesnt render :(

--- src/main/java/inf112/gunit/main/Main.java
+++ src/main/java/inf112/gunit/main/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "RoboRally";
-        cfg.width = 480;
-        cfg.height = 320;
+        cfg.width = 500;
+        cfg.height = 500;
 
         new LwjglApplication(new Game(), cfg);
     }

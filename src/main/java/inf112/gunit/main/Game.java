adbee27c0f420107497746e7c6b62c854e61feaf
Bo Aanes <boaanes@gmail.com>
2020-02-04 10:45:38 +0100

    :art: Window dimensions
    Added window dimension variables

--- src/main/java/inf112/gunit/main/Game.java
+++ src/main/java/inf112/gunit/main/Game.java
@@ -3,6 +3,7 @@ package inf112.gunit.main;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
@@ -20,9 +21,17 @@ public class Game implements ApplicationListener {
     public void create() {
         tiledMap = new TmxMapLoader().load("src/assets/tiles.tmx");
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+        MapProperties props = tiledMap.getProperties();
+
+        int tileWidth = props.get("tilewidth", Integer.class);
+        int tileHeight = props.get("tileheight", Integer.class);
+        int mapWidth = props.get("width", Integer.class);
+        int mapHeight = props.get("height", Integer.class);
+
         Board = (TiledMapTileLayer) tiledMap.getLayers().get("Board");
+
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 1500, 1500);
+        camera.setToOrtho(false, tileWidth*mapWidth, tileWidth*mapHeight);
         camera.translate(2.5f, 0);
         camera.update();
 

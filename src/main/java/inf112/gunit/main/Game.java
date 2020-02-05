65fe53bd35fbd953026f0d8e5b6a441ebb1f3b63
Bo Aanes <boaanes@gmail.com>
2020-02-05 20:06:20 +0100

    :sparkles: Player status, cleaned up files
    Flags and hole now "affect" player, removed unused assets and test

--- src/main/java/inf112/gunit/main/Game.java
+++ src/main/java/inf112/gunit/main/Game.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -19,16 +18,24 @@ import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 
 public class Game extends InputAdapter implements ApplicationListener {
     private TiledMap tiledMap;
+
     private TiledMapTileLayer layerBoard;
     private TiledMapTileLayer layerPlayer;
+    private TiledMapTileLayer layerHole;
+    private TiledMapTileLayer layerFlag1;
+    private TiledMapTileLayer layerFlag2;
+    private TiledMapTileLayer layerFlag3;
+    private TiledMapTileLayer layerFlag4;
+
     private MapProperties props;
 
     private OrthographicCamera camera;
     private OrthogonalTiledMapRenderer tileRenderer;
     
-    private TiledMapRenderer tiledMapRenderer;
-    
     private Cell playerCell;
+    private Cell playerDiedCell;
+    private Cell playerWonCell;
+
     private Vector2 playerPosition;
 
     @Override
@@ -36,9 +43,8 @@ public class Game extends InputAdapter implements ApplicationListener {
         Gdx.input.setInputProcessor(this);
 
         tiledMap = new TmxMapLoader().load("src/assets/tiles_80x80.tmx");
-        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-        props = tiledMap.getProperties();
 
+        props = tiledMap.getProperties();
         int tileWidth = props.get("tilewidth", Integer.class);
         int tileHeight = props.get("tileheight", Integer.class);
         int mapWidth = props.get("width", Integer.class);
@@ -46,17 +52,26 @@ public class Game extends InputAdapter implements ApplicationListener {
 
         layerBoard = (TiledMapTileLayer) tiledMap.getLayers().get("board");
         layerPlayer = (TiledMapTileLayer) tiledMap.getLayers().get("player");
+        layerHole = (TiledMapTileLayer) tiledMap.getLayers().get("hole");
+        layerFlag1 = (TiledMapTileLayer) tiledMap.getLayers().get("flag1");
+        layerFlag2 = (TiledMapTileLayer) tiledMap.getLayers().get("flag2");
+        layerFlag3 = (TiledMapTileLayer) tiledMap.getLayers().get("flag3");
+        layerFlag4 = (TiledMapTileLayer) tiledMap.getLayers().get("flag4");
 
-        Texture playerTexture = new Texture("src/assets/player_16x16.png");
+        Texture playerTexture = new Texture("src/assets/players_16x16.png");
         TextureRegion[][] playerTextureSplit = TextureRegion.split(playerTexture, tileWidth, tileHeight);
-        playerCell = new Cell().setTile(new StaticTiledMapTile(playerTextureSplit[0][0]));
+
+        playerCell = new Cell().setTile(new StaticTiledMapTile(playerTextureSplit[0][3]));
+        playerDiedCell = new Cell().setTile(new StaticTiledMapTile(playerTextureSplit[0][0]));
+        playerWonCell = new Cell().setTile(new StaticTiledMapTile(playerTextureSplit[0][1]));
+
         playerPosition = new Vector2(0, 0);
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, tileWidth*mapWidth, tileHeight*mapHeight);
         camera.update();
 
-        tileRenderer = new OrthogonalTiledMapRenderer(tiledMap, (float) 1/16*16);
+        tileRenderer = new OrthogonalTiledMapRenderer(tiledMap, (float) 1/tileWidth*tileHeight);
         tileRenderer.setView(camera);
     }
 
@@ -68,7 +83,18 @@ public class Game extends InputAdapter implements ApplicationListener {
     @Override
     public void render() {
         Gdx.gl.glClearColor(1, 0, 0, 1);
-        layerPlayer.setCell((int) playerPosition.x, (int) playerPosition.y, playerCell);
+
+        if (layerHole.getCell((int) playerPosition.x, (int) playerPosition.y) != null) {
+            layerPlayer.setCell((int) playerPosition.x, (int) playerPosition.y, playerDiedCell);
+        } else if (layerFlag1.getCell((int) playerPosition.x, (int) playerPosition.y) != null ||
+                layerFlag2.getCell((int) playerPosition.x, (int) playerPosition.y) != null ||
+                layerFlag3.getCell((int) playerPosition.x, (int) playerPosition.y) != null ||
+                layerFlag4.getCell((int) playerPosition.x, (int) playerPosition.y) != null) {
+            layerPlayer.setCell((int) playerPosition.x, (int) playerPosition.y, playerWonCell);
+        } else {
+            layerPlayer.setCell((int) playerPosition.x, (int) playerPosition.y, playerCell);
+        }
+
         tileRenderer.render();
     }
 
@@ -88,7 +114,7 @@ public class Game extends InputAdapter implements ApplicationListener {
     }
 
     @Override
-    public boolean keyDown(int keyCode) {
+    public boolean keyUp(int keyCode) {
         layerPlayer.setCell((int) playerPosition.x, (int) playerPosition.y, null);
 
         int x = (int) playerPosition.x;

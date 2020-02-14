95ea4ebb2d5f3d63d1a94e015ba6fc0010ced2f9
Bo Aanes <boaanes@gmail.com>
2020-02-14 12:01:22 +0100

    :sparkles: Program card
    Implemented simple superclass for program card

---
+++ src/main/java/inf112/gunit/player/card/ProgramCard.java
@@ -0,0 +1,15 @@
+package inf112.gunit.player.card;
+
+public class ProgramCard {
+    
+    private int priority;
+
+    public ProgramCard(int priority) {
+        this.priority = priority;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+}

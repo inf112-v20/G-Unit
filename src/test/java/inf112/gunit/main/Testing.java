1564d4a65110b5e93d60b2018806ecb78a2ad048
erik andal <erikandal@pc123-197.eduroam.uib.no>
2020-02-28 10:48:57 +0100

    Created test skeleton

---
+++ src/test/java/inf112/gunit/main/Testing.java
@@ -0,0 +1,126 @@
+package inf112.gunit.main;
+
+import org.junit.Test;
+
+public class Testing {
+
+    /**
+     * Test to see that all players have been dealt cards.
+     */
+    @Test
+    public void testToSeeIfAllTheCardsHaveBeenDealt() {
+
+    }
+
+    /**
+     * Test to check if each player has picked five cards each.
+     */
+    @Test
+    public void testToCheckIfPlayersHavePickedFiveCardsEach(){
+
+    }
+
+    /**
+     * Test to check that the number of players are between 1-4.
+     */
+    @Test
+    public void testIfNumberOfPlayersAreBetweenOneAndFour(){
+
+    }
+
+    /**
+     * Test to check if the players robot have turned in the right manner,
+     * relative to the card played.
+     */
+    @Test
+    public void testIfRobotsHaveTurnedTheRightWay(){
+
+    }
+
+    /**
+     * Test to check if damage taken on a robot
+     * is being registered correctly onto player.
+     */
+    @Test
+    public void testHaveDamageBeenRegistered(){
+
+    }
+
+    /**
+     * Test to check if somebody has won.
+     */
+    @Test
+    public void testHaveSomebodyWon(){
+
+    }
+
+    /**
+     * Test to check if robot has been repositioned
+     * correctly after falling into a pit.
+     */
+    @Test
+    public void testHaveRobotBeenRepositionedCorrectlyAfterPit(){
+
+    }
+
+    /**
+     * Test to check if robot has been repositioned
+     * correctly after interaction with a pusher.
+     */
+    @Test
+    public void testHaveRobotBeenRepositionedCorrectlyAfterPusher(){
+
+    }
+
+    /**
+     * Test to check if a robot has gotten all
+     * "Damage Markers" removed after repairing.
+     */
+    @Test
+    public void testHaveRobotRepairedCompletely(){
+
+    }
+
+    /**
+     * Test to check if a player has gotten
+     * one life token removed after dying.
+     */
+    @Test
+    public void testIfPlayerLostLifeToken(){
+
+    }
+
+    /**
+     * Test to check if Backup Memory Tile
+     * have been moved after last reposition.
+     */
+    @Test
+    public void testHaveBackupMemoryTileBeenMoved(){
+
+    }
+
+    /**
+     * Test to check if a player has picked up a
+     * flag that his/her robot landed on.
+     */
+    @Test
+    public void testHaveRobotPickedUpFlag(){
+
+    }
+
+    /**
+     * Test to check if a robot have been given an upgrade.
+     */
+    @Test
+    public void testHaveRobotBeenUpgraded(){
+
+    }
+
+    /**
+     * Test to check if the timer has started.
+     */
+    @Test
+    public void testDidTimerStart(){
+
+    }
+}

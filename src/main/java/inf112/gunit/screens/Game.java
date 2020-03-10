1e3b1a62eba80126e46309f9a347f3239ca23f3a
Bo Aanes <boaanes@gmail.com>
2020-03-10 12:27:57 +0100

    :construction: Adding support for up to 4 players
    Currently very WIP, need to handle players/robots walking out of bounds

--- src/main/java/inf112/gunit/screens/Game.java
+++ src/main/java/inf112/gunit/screens/Game.java
@@ -1,6 +1,8 @@
 package inf112.gunit.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -9,6 +11,8 @@ import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import inf112.gunit.board.Direction;
 import inf112.gunit.main.Main;
 import inf112.gunit.player.Player;
 
@@ -16,7 +20,7 @@ import inf112.gunit.player.Player;
  * The Game class is a screen which is rendered
  * when the Play-button is pressed in menu
  */
-public class Game implements Screen {
+public class Game extends InputAdapter implements Screen {
 
     public static TextureRegion[][] spriteSheet;
 
@@ -25,22 +29,27 @@ public class Game implements Screen {
     private Main main;
     private TiledMap map;
     private TiledMapTileLayer[] layers;
+    private MapProperties props;
 
     private Player[] players;
-
-    private Player player;
+    private Player mainPlayer;
 
     private OrthographicCamera camera;
     private OrthogonalTiledMapRenderer tileRenderer;
+
+    private int cardIdx;
 
     /**
      * The Game constructor
      * @param main takes a main
      * @param map takes a TiledMap as the board
      */
-    public Game(Main main, TiledMap map) {
+    public Game(Main main, TiledMap map, int numOfPlayers) {
         this.main = main;
         this.map = map;
+        this.players = new Player[numOfPlayers];
+
+        cardIdx = 0;
 
         MapLayers mapLayers = map.getLayers();
         layers = new TiledMapTileLayer[mapLayers.size()];
@@ -50,14 +59,18 @@ public class Game implements Screen {
 
         tick = 0;
 
-        MapProperties props = map.getProperties();
+        props = map.getProperties();
         int mapWidth = map.getProperties().get("width", Integer.class);
         int mapHeight = map.getProperties().get("height", Integer.class);
         int tileWidth = map.getProperties().get("tilewidth", Integer.class);
         int tileHeight = map.getProperties().get("tileheight", Integer.class);
 
-        player = new Player(map);
-        Gdx.input.setInputProcessor(player);
+        for (int i = 0; i < numOfPlayers; i++) {
+            Player p = new Player(map, i);
+            players[i] = p;
+        }
+
+        mainPlayer = players[0];
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, mapWidth * tileWidth, mapHeight * tileHeight);
@@ -65,6 +78,8 @@ public class Game implements Screen {
 
         tileRenderer = new OrthogonalTiledMapRenderer(map, (float) 1/ tileWidth * tileHeight);
         tileRenderer.setView(camera);
+
+        Gdx.input.setInputProcessor(this);
     }
 
     @Override
@@ -77,24 +92,90 @@ public class Game implements Screen {
         Gdx.gl.glClearColor(1,0,0,1);
 
         // update the player
-        player.update();
+        for (Player player : players) {
+            player.update();
+
+            if (tick % 30 == 0) {
+                for (int i = 0; i < layers.length; i++) {
+                    if (layers[i].getName().equals("rotator_clockwise") && layers[i].getCell((int) player.getPositionX(), (int) player.getPositionY()) != null) {
+                        player.rotate(true, 1);
+                    } else if (layers[i].getName().equals("rotator_counter_clockwise") && layers[i].getCell((int) player.getPositionX(), (int) player.getPositionY()) != null) {
+                        player.rotate(false, 1);
+                    }
+                }
+            }
+        }
 
         // render the tile-map
         tileRenderer.setView(camera);
         tileRenderer.render();
 
         // update game mechanics every 0.5 seconds (game runs at 60 fps)
-        if (tick % 30 == 0) {
-            for (int i = 0; i < layers.length; i++) {
-                if (layers[i].getName().equals("rotator_clockwise") && layers[i].getCell((int) player.getPositionX(), (int) player.getPositionY()) != null) {
-                    player.rotate(true, 1);
-                } else if (layers[i].getName().equals("rotator_counter_clockwise") && layers[i].getCell((int) player.getPositionX(), (int) player.getPositionY()) != null) {
-                    player.rotate(false, 1);
-                }
-            }
-        }
 
         tick++; // increase the game tick
+    }
+
+    // key-listener currently used for testing
+    @Override
+    public boolean keyUp(int keyCode) {
+        Vector2 position = mainPlayer.getPosition();
+        TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get("player_0");
+        Direction dir = mainPlayer.getDirection();
+
+        int x = (int) position.x;
+        int y = (int) position.y;
+
+        switch (keyCode) {
+            case Input.Keys.LEFT:
+                if (x - 1 < 0 || x - 1 >= props.get("width", Integer.class))
+                    return false;
+                else {
+                    layer.setCell((int) position.x, (int) position.y, null);
+                    mainPlayer.setDirection(Direction.WEST);
+                    position.set(x - 1, y);
+                    return true;
+                }
+
+            case Input.Keys.RIGHT:
+                if (x + 1 < 0 || x + 1 >= props.get("width", Integer.class))
+                    return false;
+                else {
+                    layer.setCell((int) position.x, (int) position.y, null);
+                    mainPlayer.setDirection(Direction.EAST);
+                    position.set(x + 1, y);
+                    return true;
+                }
+
+            case Input.Keys.UP:
+                if (y + 1 < 0 || y + 1 >= props.get("height", Integer.class))
+                    return false;
+                else {
+                    layer.setCell((int) position.x, (int) position.y, null);
+                    mainPlayer.setDirection(Direction.NORTH);
+                    position.set(x, y + 1);
+                    return true;
+                }
+
+            case Input.Keys.DOWN:
+                if (y - 1 < 0 || y - 1 >= props.get("height", Integer.class))
+                    return false;
+                else {
+                    layer.setCell((int) position.x, (int) position.y, null);
+                    mainPlayer.setDirection(Direction.SOUTH);
+                    position.set(x, y - 1);
+                    return true;
+                }
+
+            case Input.Keys.SPACE:
+                for (Player p : players) {
+                    p.doTurn(p.getProgram()[cardIdx % 5]);
+                }
+                cardIdx++;
+                return true;
+
+            default:
+                return false;
+        }
     }
 
     @Override

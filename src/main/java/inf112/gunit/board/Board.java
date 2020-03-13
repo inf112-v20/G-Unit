35b7a615a57eef0a5536b801e89822f3e23dfec5
Bo Aanes <boaanes@gmail.com>
2020-03-13 12:36:46 +0100

    :construction: Board and mechanics
    Cleaned up Board class, write logic and mechanics here

--- src/main/java/inf112/gunit/board/Board.java
+++ src/main/java/inf112/gunit/board/Board.java
@@ -8,59 +8,16 @@ import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+import inf112.gunit.screens.Game;
 
+/**
+ * Implement logic and mechanics here
+ */
 public class Board {
 
-    private TiledMap tiledMap;
+    private Game game;
 
-    private TextureRegion[][] sprites;
-
-    private int mapWidth;
-    private int mapHeight;
-    private int tileWidth;
-    private int tileHeight;
-
-    public Board(String boardSrc) {
-        tiledMap = new TmxMapLoader().load(boardSrc);
-
-        mapWidth = tiledMap.getProperties().get("width", Integer.class);
-        mapHeight = tiledMap.getProperties().get("height", Integer.class);
-        tileWidth = tiledMap.getProperties().get("tilewidth", Integer.class);
-        tileHeight = tiledMap.getProperties().get("tileheight", Integer.class);
-
-        TiledMapTileLayer testLayer = new TiledMapTileLayer(mapWidth,mapHeight,300,300);
-
-        Texture texture = new Texture("src/assets/tiles.png");
-        sprites = TextureRegion.split(texture, tileWidth, tileHeight);
-
-        TiledMapTileLayer.Cell testCell = new TiledMapTileLayer.Cell().setTile(new StaticTiledMapTile(sprites[11][0]));
-
-        testLayer.setCell(0,9,testCell);
-
-        tiledMap.getLayers().add(testLayer);
-    }
-
-    public TiledMap getTiledMap() {
-        return tiledMap;
-    }
-
-    public int getMapWidth() {
-        return mapWidth;
-    }
-
-    public int getMapHeight() {
-        return mapHeight;
-    }
-
-    public int getTileWidth() {
-        return tileWidth;
-    }
-
-    public int getTileHeight() {
-        return tileHeight;
-    }
-
-    public void update() {
-        //TODO
+    public Board(Game game) {
+        this.game = game;
     }
 }

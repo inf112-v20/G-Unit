0472ab6ea06b298c552400ad0f015eeaf85574b1
Elias Djupesland <eliasdjup@hotmail.com>
2020-03-20 11:01:43 +0100

    :sparkles: Started working on netcode
    Working on Server, Client and screens for joining/hosting

---
+++ src/main/java/inf112/gunit/screens/HostScreen.java
@@ -0,0 +1,46 @@
+package inf112.gunit.screens;
+
+import com.badlogic.gdx.Screen;
+import inf112.gunit.main.Main;
+
+public class HostScreen implements Screen {
+
+    public HostScreen(Main main) {
+        this.main = main;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}

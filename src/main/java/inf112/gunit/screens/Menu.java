3cd42c4e07bb6dacf5c8db1a2accb0ad639086db
eliasdjup <eliasdjup@gmail.com>
2020-05-02 18:48:46 +0200

    Removed netcode and updated menu for new features.

--- src/main/java/inf112/gunit/screens/Menu.java
+++ src/main/java/inf112/gunit/screens/Menu.java
@@ -3,14 +3,13 @@ package inf112.gunit.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Slider;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -63,10 +62,14 @@ public class Menu extends RRScreen {
         mainTable.top();
         skin.getFont("font").getData().setScale(3);
 
+        Texture logotexture = new Texture(Gdx.files.internal("logo.png"));
+        Sprite logosprite = new Sprite(logotexture);
+        logosprite.setColor(1, 0, 0, 0.2f);
+        Image logo = new Image(logosprite);
+
         final TextButton easyplayButton = new TextButton("Easy", skin);
-        easyplayButton.getLabel().setFontScale(3);
         final TextButton hardplayButton = new TextButton("Hard", skin);
-        hardplayButton.getLabel().setFontScale(3);
+        final TextButton quitButton = new TextButton("Quit", skin);
         numplayers = new Slider(2,4,1,false, skin);
         numplayerstext = new TextField("Number of AI opponents: ", skin);
         numplayerstext.setAlignment(Align.center);
@@ -92,7 +95,7 @@ public class Menu extends RRScreen {
                 main.setScreen(new Game(main, (int) numplayers.getValue()));
             }
         });
-        hardplayButton.addListener(new ClickListener(){
+        quitButton.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 Gdx.app.exit();
@@ -102,6 +105,8 @@ public class Menu extends RRScreen {
         int padding = 50;
         //Add buttons to table
         mainTable.row();
+        mainTable.add(logo);
+        mainTable.row();
         mainTable.add(easyplayButton).width(400).height(100).pad(padding);
         mainTable.row();
         mainTable.add(hardplayButton).width(400).height(100).pad(padding);
@@ -109,6 +114,8 @@ public class Menu extends RRScreen {
         mainTable.add(numplayerstext).width(700).height(100).pad(padding);
         mainTable.row();
         mainTable.add(numplayers).width(400);
+        mainTable.row();
+        mainTable.add(quitButton).width(400).height(100).pad(padding);
 
         mainTable.center();
 
@@ -118,6 +125,7 @@ public class Menu extends RRScreen {
 
     @Override
     public void render(float v) {
+
         Gdx.gl.glClearColor(.1f, .12f, .16f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
